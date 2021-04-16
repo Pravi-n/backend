a676508f8e670978895b4b9402bc72cfc86f5bbd
Pravi-n <poshmanipravin1@gmail.com>
2021-04-16 18:32:03 +0530

    okk

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3001;
+const port = 3005;
 //provide schema for dataBase
-const userModel = require("./models/UserData");
+const projectsRouter = require('./routes/project_router');
+const logsRouter = require('./Routes/log_router')
+
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());//help to communicate with api which we build eg. all react code is like a api for us now
 
-mongoose.connect("our_mongo_link",{
+mongoose.connect("mongodb://localhost/projects",{
 	 useNewUrlParser: true,
 	 useUnifiedTopology: true,
 }).then(()=>{
@@ -24,7 +26,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/api', movieRouter)
+app.use('/api', projectsRouter)
+app.use('/log', logsRouter)
 
 app.listen(port, ()=>{
 	console.log("server is runing on "+port);
--- project/models/project_model.js
+++ project/models/project_model.js
@@ -3,13 +3,25 @@ const Schema = mongoose.Schema
 
 const Project = new Schema(
     {
-        projectName: { type: String, required: true },
-        noOfMembers: { type: Number, required: true },
-        doc: { type: Date, required: true },
-        noOfTeams : {type : Number, required :true},
-        edoc: { type: Date, required: true },
+        projectName: { type: String,
+             required: true },
+        description:{  type: String,
+            required:true },
+        startDate: { type: Date,
+             required: true },
+        endDate: { type: Date,
+            required: true },  
+        projectStatus : {type : Number,
+             required :true},
+        teamMembers: [Number],
+        createdDate: { type: Date,
+            required: true },
+        updatedDate: { type: Date,
+                required: true },
+        deletedDate: { type: Date,
+                    required: true },
     },
-    { timestamps: true },
+
 )
 
 module.exports = mongoose.model('projects', Project)
